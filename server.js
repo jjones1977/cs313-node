21d000f0ba98c53f0f78d4c4e1897b807c074c7a
jjones1977 <JenniJones9977@gmail.com>
2020-03-07 20:38:19 -0800

    added postage page

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set("views", "views");
+app.set('views', __dirname + '/views');
 app.set("view engine", "ejs");
 
 app.listen(8000, function() {
