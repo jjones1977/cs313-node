e76b6fd63a34afc53a5b9c7129536bc50d0fcd15
jjones1977 <JenniJones9977@gmail.com>
2020-03-07 22:52:02 -0800

    added postage page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.static(__dirname, '/public')));
+app.use(express.static(__dirname, '/public'));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
